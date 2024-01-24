477c6dd19cd462ef6d2899d6330a333c11c88a30
Max6468 <dalfonsoto@gmail.com>
2024-01-24 21:57:53 +0100

    Anuncios, cabecera, pie y menu

--- js/header.js
+++ js/header.js
@@ -2,8 +2,17 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("titulo").style.fontSize = "40px";
+    document.getElementById("header").style.padding = "0px 10px";
+    document.getElementById("left-sidebar").style.marginTop = "99px";
+    document.getElementById("right-sidebar").style.marginTop = "99px";
+    document.getElementById("main").style.marginTop = "99px";
+
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("titulo").style.fontSize = "90px";
+    document.getElementById("header").style.padding = "120px 10px";
+    document.getElementById("left-sidebar").style.marginTop = "464px";
+    document.getElementById("right-sidebar").style.marginTop = "464px";
+
   }
 }
